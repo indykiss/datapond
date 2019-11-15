4d6433918f9cc9f7f5ed1441d5d4c44cfcfab27d
indykiss <indirak@alumni.princeton.edu>
2019-11-14 22:26:47 -0500

    clean html

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_03_193951) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
