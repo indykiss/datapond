2b771d4e9e5b0f8f1870124a6b63c1eca235f31a
indykiss <indirak@alumni.princeton.edu>
2020-01-19 21:19:07 -0500

    Fix login issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
     has_many :favorites
     # users have many fav_dat_packs through favorite: 
     has_many :favorited_data_packages, through: :favorites, source: :data_package
-    
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 80 }
-    has_secure_password
-
+    # validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 80 }
+    has_secure_password validations: false
+    validates :password, presence: true, length: { minimum: 1 }
 
 
 end
